df7f9ca15d8815b319d7f9bf70711de54394ed4e
Cederic Bosmans <bosmansc6@gmail.com>
2019-05-21 20:55:58 +0200

    login

---
+++ app/src/main/java/bosmans/frigo/LoginActivity.java
@@ -0,0 +1,53 @@
+package bosmans.frigo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText password;
+    private int counter = 3;
+    private Button login;
+    private TextView info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        name = (EditText)findViewById(R.id.et_naam);
+        password = (EditText)findViewById((R.id.et_password));
+        login = (Button)findViewById(R.id.btn_login);
+        info = (TextView)findViewById(R.id.tvInfo);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(name.getText().toString(), password.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String userName, String userPassword){
+        if(userPassword.equals("FL")){
+            Intent intent =  new Intent(LoginActivity.this, MainActivity.class);
+            intent.putExtra("userName", userName);
+            startActivity(intent);
+        }
+        else {
+            counter--;
+
+            info.setText("Resterende pogingen: " + String.valueOf(counter));
+            if(counter == 0){
+                login.setEnabled(false);
+            }
+        }
+    }
+}
--- app/src/main/java/bosmans/frigo/MainActivity.java
+++ app/src/main/java/bosmans/frigo/MainActivity.java
@@ -1,5 +1,6 @@
 package bosmans.frigo;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,27 +8,52 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import bosmans.frigo.R;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
-    Button buttonListGames,buttonNextGame, buttonAanwezigheid, buttonPresentPlayers;
+    Button buttonListGames, buttonAanwezigheid, buttonPresentPlayers;
+    TextView nextGame;
+    ProgressDialog loading;
+    String volgendePloeg;
+    String userName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-
         buttonListGames = findViewById(R.id.btn_viewGames);
-        buttonNextGame = findViewById(R.id.btn_nextGame);
         buttonAanwezigheid = findViewById(R.id.btn_aanwezigheid);
         buttonPresentPlayers = findViewById(R.id.btn_presentPlayers);
+        nextGame = (TextView)findViewById(R.id.tv_nextGame);
         buttonListGames.setOnClickListener(this);
-        buttonNextGame.setOnClickListener(this);
         buttonAanwezigheid.setOnClickListener(this);
         buttonPresentPlayers.setOnClickListener(this);
 
+        Intent intentLogin = getIntent();
+        String userName = intentLogin.getStringExtra("userName");
+        getGame("start", userName); // method to present the next game at the top of the screen
         Log.e("TAG", "Message");
     }
 
@@ -40,16 +66,12 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             startActivity(intent);
         }
 
-        if(v==buttonNextGame){
-
-            Intent intent = new Intent(getApplicationContext(),NextGame.class);
-            startActivity(intent);
-        }
-
         if(v==buttonAanwezigheid){
 
-            Intent intent = new Intent(getApplicationContext(), AddPlayerToGame.class);
-            startActivity(intent);
+            Intent intentLogin = getIntent();
+            String userName = intentLogin.getStringExtra("userName");
+            getGame("aanwezigheid", userName);
+            buttonAanwezigheid.setEnabled(false);
         }
 
         if(v==buttonPresentPlayers){
@@ -58,5 +80,118 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             startActivity(intent);
         }
 
+    }
+
+    private void getGame(final String string, final String userName){
+
+        loading =  ProgressDialog.show(this,"Loading","Even geduld " + userName,false,true);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://script.google.com/macros/s/AKfycbz1FUSOUSlfWxaHUbnf0N6zMA_3xF_UqMl1PtEKQhjlxwQOf6w/exec?action=getGames",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        parseItems(response, string, userName);
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+        int socketTimeOut = 50000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(stringRequest);
+
+    }
+
+    private void parseItems(String jsonResposnce, String string, String userName) {
+        try {
+            JSONObject jobj = new JSONObject(jsonResposnce);
+            JSONArray jarray = jobj.getJSONArray("games");
+
+
+            for (int i = 0; i < jarray.length(); i++) {
+
+                JSONObject jo = jarray.getJSONObject(i);
+                String gameDate = jo.getString("Date");
+                String ploeg = jo.getString("Ploeg");
+                String plaats = jo.getString("Plaats");
+
+                SimpleDateFormat sourceFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+                SimpleDateFormat destFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                Date convertedDate = sourceFormat.parse(gameDate);
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(convertedDate);
+                cal.add(Calendar.HOUR, 2);
+                convertedDate = cal.getTime();
+                Date date = new Date();
+
+                if(date.compareTo(convertedDate) < 0) // Return value > 0 , if convertedDate is after the date argument.
+                {
+
+                    String gameConvDate = destFormat.format(convertedDate);
+
+                    if(string == "start")
+                    nextGame.setText("Volgende wedstrijd  \n \n" + ploeg + " \n \n" + gameConvDate );
+
+                    if(string == "aanwezigheid") {
+                        volgendePloeg = ploeg;
+                        addNameToSheet(userName); // here the name is added to the sheet based on the next opponent
+                    }
+                    break;
+                }
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        loading.dismiss();
+    }
+    //This is the part where data is transferred from Your Android phone to Sheet by using HTTP Rest API calls
+
+    private void addNameToSheet(final String userName) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://script.google.com/macros/s/AKfycbzxsTjbi4wuds0wBuQW3PrLMaEvbnAD9_-X4ROOn7wGBIZoYEA/exec",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Toast.makeText(MainActivity.this,response,Toast.LENGTH_LONG).show();
+                        loading.dismiss();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> parmas = new HashMap<>();
+
+                //here we pass params
+                parmas.put("action","addPlayer");
+                parmas.put("speler",userName);
+                parmas.put("ploeg",volgendePloeg.replaceAll(" ","").replaceAll("'",""));
+
+                return parmas;
+            }
+        };
+
+        int socketTimeOut = 50000;// u can change this .. here it is 50 seconds
+        RetryPolicy retryPolicy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(retryPolicy);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(stringRequest);
     }
 }
